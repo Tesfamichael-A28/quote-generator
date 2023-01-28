1be263875ffbc1ee9daefdfc6932129529ad2fbd
Tesfamichael.A <tesfa722@gmail.com>
2023-01-28 12:14:51 +0300

    Loading Animation

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+//hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 // Show new quote
 function newQuote() {
+  loading();
   // pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // check if author field is blank
@@ -21,11 +33,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  //set Quote, Hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
